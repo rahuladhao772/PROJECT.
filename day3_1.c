50e1152aa3d91da21c3fc46dcbc54303e9ea90dc
rahuladhao772 <124015838+rahuladhao772@users.noreply.github.com>
2023-03-27 15:21:05 +0530

    manual of project

---
+++ day3_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    int val=50;
+    int n1=10,n2=20;
+    printf("VALUE : %d",val);
+    printf("N1 = %d N2 = %d ",n1,n2);
+    return 0;
+    
+}
